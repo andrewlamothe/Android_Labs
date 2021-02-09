f1ab6fc1ba288f84fbecff326b171b14e043389d
Andrew <dfsd@fsdfs>
2021-02-09 14:57:40 -0500

    Step 1

--- app/src/main/java/com/cst2335/lamo0241/MainActivity.java
+++ app/src/main/java/com/cst2335/lamo0241/MainActivity.java
@@ -2,47 +2,45 @@ package com.cst2335.lamo0241;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.StringSearch;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 private int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView myTextView = findViewById(R.id.text1);
-        TextView checktext = findViewById(R.id.checked);
-        EditText text2 = findViewById(R.id.text2);
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        CheckBox checker = findViewById(R.id.checker);
+        setContentView(R.layout.activity_main_linear);
+        Switch switcher = findViewById(R.id.switcher);
+        Button button1 = findViewById(R.id.button);
+
         button1.setOnClickListener(x -> {
-                counter++;
-                text2.setText("Button Has been pressed " + counter + " times");
-                Toast.makeText(this,"HELLO!", Toast.LENGTH_LONG).show();
+               Toast.makeText(this,getString(R.string.button_click), Toast.LENGTH_LONG).show();
             }
         );
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View r) {
-                counter--;
-                text2.setText("Button Has been pressed " + counter + " times");
-            }
-        });
-
-        checker.setOnCheckedChangeListener((checkbox, isChecked) ->{
+        switcher.setOnCheckedChangeListener((checkbox, isChecked) ->{
             if (isChecked){
-                checktext.setText("Checked!");
+               Snackbar switch_on = Snackbar.make(findViewById(R.id.switcher) , getString(R.string.snackbar_on), Snackbar.LENGTH_LONG);
+                switch_on.setAction(R.string.undo_string, click -> checkbox.setChecked(!isChecked));
+                switch_on.show();
             }
-            else checktext.setText("Unchecked!");
+            else {
+               Snackbar switch_off = Snackbar.make(findViewById(R.id.switcher) , getString(R.string.snackbar_off), Snackbar.LENGTH_LONG);
+
+                switch_off.show();
+            }
         });
 
     }
